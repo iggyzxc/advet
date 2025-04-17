73f656db5b0c8d8eb1a4453bd1f0184126e56118
Iggy <ignaciomacam@gmail.com>
2025-04-17 14:03:39 +0800

    project initial structure

---
+++ src/main/java/com/macadev/advet/AdvetApplication.java
@@ -0,0 +1,13 @@
+package com.macadev.advet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdvetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdvetApplication.class, args);
+	}
+
+}
